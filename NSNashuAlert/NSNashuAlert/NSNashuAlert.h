6ac38951a3b3989ff1d2229ec96e0925dd1a3d03
George Velesca <gvelesca@MOVIALs-Mac-mini.local>
2016-01-07 13:43:59 +0200

    created framework project

---
+++ NSNashuAlert/NSNashuAlert/NSNashuAlert.h
@@ -0,0 +1,19 @@
+//
+//  NSNashuAlert.h
+//  NSNashuAlert
+//
+//  Created by George Velesca on 1/7/16.
+//  Copyright © 2016 NSNashu.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NSNashuAlert.
+FOUNDATION_EXPORT double NSNashuAlertVersionNumber;
+
+//! Project version string for NSNashuAlert.
+FOUNDATION_EXPORT const unsigned char NSNashuAlertVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NSNashuAlert/PublicHeader.h>
+
+
